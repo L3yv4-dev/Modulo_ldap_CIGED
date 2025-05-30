805d6b32d1983909b0a06fe2e5a976457d604a7d
L3yv4-Dev <leyv4.dev@gmail.com>
2025-05-30 05:37:22 +0200

    update service auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as ldap from 'ldapjs';
 import { User } from 'src/users/user.entity';
 import { Repository } from 'typeorm';
-
 @Injectable()
 export class AuthService {
     private client: ldap.Client;
 
     constructor(
         private jwtService: JwtService,
-        @InjectRepository(User) private userRepository: Repository<User>
+        @InjectRepository(User) private userRepository: Repository<User>,
+        private configService: ConfigService,
     ){
+        const ldapUrl =  this.configService.get<string>('LDAP_URL') || 'ldap://localhost:389';
         this.client = ldap.createClient({
-            url: process.env.LDAP_URL || 'ldap://localhost:389'
+            url: ldapUrl,
         });
     }
 
@@ -28,9 +30,9 @@ export class AuthService {
                 const user = await this.userRepository.findOne({where:{username}});
 
                 if(!user){
-                    //si el usuario no existe en base de datos lo crea
-                    const newUser = this.userRepository.create({username});
-                    await this.userRepository.save(newUser);
+                    //si el usuario no existe en base de datos lo crea (preguntar)
+                    // const newUser = this.userRepository.create({username});
+                    // await this.userRepository.save(newUser);
                     // return reject('No exite el usuario');
                 }
                 const payload = {username};
